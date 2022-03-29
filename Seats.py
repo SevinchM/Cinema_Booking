1c6c6c87f37f8291b7cd135f4ce2c7c04b07a93e
SevinchM <sevinchbegim.m@asu.edu.mn>
2022-03-29 14:27:20 +0800

    finisinh

--- Seats.py
+++ Seats.py
@@ -1,97 +1,149 @@
+def booking():
 
-print("Welcome to Cinemaplex!")
+    print("Welcome to Cinemaplex!")
 
-movie_choices = {'01': 'The Batman','02':'Turning Red', '03':'Spiderman: No Way Home'}
-sessions = {'01':'10AM','02':'3PM', '03':'6PM'}
+    print("You may order a movie session and book seats here!")
 
-name = None
-movie_order = None
-seating_choice = None
-number = None
+    print()
+    #creating dictionaries for the available movies and sessions
+    movie_choices = {'01': 'The Batman','02':'Turning Red', '03':'Spiderman: No Way Home'}
+    sessions = {'01':'10AM','02':'3PM', '03':'6PM'}
 
-name = input("Please enter your name:  ")
-print(name)
+    #creating the variables
+    name = None
+    movie_order = None
+    seating_choice = None
+    number = None
+
+    #Input the name of the person booking.
+    name = input("Please enter your name:  ")
+    print("Booking under name: ", name)
     
-number = int(input("Please enter your phone number:  "))
+    print()
+    #Entering the phone number of the person booking. 
+    number = int(input("Please enter your phone number:  "))
 
-order = {}
-
-#booking the movie.
-print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
-
-print("Please choose a movie to watch.")
-for code, choice in movie_choices.items():
-    print (code, '->', choice)
-
-print()
-
-
-#validate the starter
-while movie_order not in movie_choices.keys():
-    movie_order = input ('Please, enter the movie code: ')
-    if movie_order not in movie_choices:
-        print('Wrong movie code.')
+    #validating the user input(phone number)
+    if type(number) == int:
+        print("Pnone number: ", number)
     else:
-        order[movie_order] = movie_choices[movie_order]
+        print("Invalid number entered.")
+    order = {}
 
-#booking the session
-print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+    #booking the movie.
+    print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
 
-print("Please choose a viewing session")
-for code, choice in sessions.items():
-    print (code, '->', choice)
+    print("Please choose a movie to watch.")
+    for code, choice in movie_choices.items():
+        print (code, '->', choice)
 
-print()
-session_order = None
+    print()
 
-#validate the starter
-while session_order not in sessions.keys():
-    session_order = input ('Please, enter the session code: ')
-    if session_order not in sessions:
-        print('Wrong session code.')
-    else:
-        order[session_order] = sessions[session_order]
 
-#creating a function for the seat booking
+    #validate the movie code
+    while movie_order not in movie_choices.keys():
+        movie_order = input ('Please, enter the movie code: ')
+        if movie_order not in movie_choices:
+            print('Wrong movie code.')
+        else:
+            order[movie_order] = movie_choices[movie_order]
 
-# hello, please enter your name.
-# these are the available movies today
-# these are the available sessions
-# this is the available seating
+    #booking the session
+    print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
 
-#Seats for each movie.
-movie1_seats = {'01':[],'02':[], '03':[]}
-movie2_seats = {'01':[],'02':[], '03':[]}
-movie3_seats = {'01':[],'02':[], '03':[]}
+    print("Please choose a viewing session")
+    for code, choice in sessions.items():
+        print (code, '->', choice)
 
-match movie_order:
-    case '01':
-        movie_seats = movie1_seats
-    case '02':
-        movie_seats = movie2_seats
-    case '03':
-        movie_seats = movie3_seats
-    
-#testing (we have booked seat number 2)
-movie_seats[session_order].append(2)
+    print()
+    session_order = None
 
-print("These are the available seats.")
-seat_txt = ''
-for seat_number in range(1,16):
-    if seat_number in movie_seats[session_order]:
-        seat_txt = seat_txt + 'x '
-    else:
-        seat_txt = seat_txt + str(seat_number) + ' '
-print(seat_txt)
+    #validating the session order
+    while session_order not in sessions.keys():
+        session_order = input ('Please, enter the session code: ')
+        if session_order not in sessions:
+            print('Wrong session code.')
+        else:
+            order[session_order] = sessions[session_order]
 
-print("Please choose your seat.")
+    #creating a function for the seat booking
 
-seating_choice = None
-while seating_choice not in range(1,16):
-    try:
-        seating_choice = int(input('Please, choose your seat: '))
+
+    #Seats for each movie.
+    movie1_seats = {'01':[],'02':[], '03':[]}
+    movie2_seats = {'01':[],'02':[], '03':[]}
+    movie3_seats = {'01':[],'02':[], '03':[]}
+
+    match movie_order:
+        case '01':
+            movie_seats = movie1_seats
+        case '02':
+            movie_seats = movie2_seats
+        case '03':
+            movie_seats = movie3_seats
         
-    except:
-        print ("Wrong seat")
-        seating_choice = None
+    #testing (we have booked seat number 2)
 
+    print()
+
+    print("These are the available seats.")
+    seat_txt = ''
+    for seat_number in range(1,16):
+        if seat_number in movie_seats[session_order]:
+            seat_txt = seat_txt + 'x '
+        else:
+            seat_txt = seat_txt + str(seat_number) + ' '
+    print(seat_txt)
+
+    print("Please choose your seat.")
+
+    seating_choice = None
+    while seating_choice not in range(1,16):
+        try:
+            seating_choice = int(input('Select seat: '))
+            
+        except:
+            print ("Wrong seat")
+            seating_choice = None
+
+    movie_seats[session_order].append(seating_choice)
+
+
+    print()
+    seat_txt = ''
+    for seat_number in range(1,16):
+        if seat_number in movie_seats[session_order]:
+            seat_txt = seat_txt + 'x '
+        else:
+            seat_txt = seat_txt + str(seat_number) + ' '
+    print("Your seating:")
+    print(seat_txt)
+
+    print()
+
+    print(f"Your full order: {movie_order},{session_order},{seating_choice}")
+
+    print()
+    place_order = ''
+
+
+    while place_order.lower() not in ('yes', 'no'):
+        place_order = input ('Would you like to place your order (yes/no)? ')
+
+    if place_order == 'yes':
+        print ('Your order is being processed')
+    else:
+        print ('Your order has been cancelled')
+
+    print()
+    new_order = ''
+
+    while new_order.lower() not in ('yes', 'no'):
+        new_order = input ('Would you like to book again (yes/no)? ')
+
+    if new_order == 'yes':
+        booking()
+    else:
+        print("Thank you for using our services!")
+        
+booking()
